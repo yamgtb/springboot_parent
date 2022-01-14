add2dc0ca3f6b64e47d92ca905e8acc3dd4f46cb
yuam <781322878@qq.com>
2022-01-14 23:27:29 +0800

    springboot项目父工程初始化

---
+++ src/main/java/cn/yam/boot/SpringbootParentApplication.java
@@ -0,0 +1,13 @@
+package cn.yam.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/yam/boot/SpringbootParentApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yam.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
